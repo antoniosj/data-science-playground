1153c8cf4dce1436fabd33e5f744ae540afa025a
antoniosj <antonio.silvajr23@gmail.com>
2018-12-04 23:00:43 -0300

    import dataset and create iloc

--- machine-learning-az/data_processing_template.py
+++ machine-learning-az/data_processing_template.py
@@ -5,3 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# importing dataset
+dataset = pd.read_csv('Data.csv')
+# creates matrix of independents variables until last column minus 1
+X = dataset.iloc[:, :-1].values
+
+# creates matrix of dependent variable. The last column.
+lastColumn = 3
+Y = dataset.iloc[:, lastColumn].values
