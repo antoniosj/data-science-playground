96e8e109813d2c4e885371c597272894f55395ef
antoniosj <antonio.silvajr23@gmail.com>
2018-12-06 00:04:40 -0300

    add encoding categorial data

--- machine-learning-az/data_processing_template.py
+++ machine-learning-az/data_processing_template.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.preprocessing import Imputer
+
 
 # importing dataset
 dataset = pd.read_csv('Data.csv')
@@ -16,7 +16,28 @@ lastColumn = 3
 Y = dataset.iloc[:, lastColumn].values
 
 # tirar a media para preencher valor faltando
+from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 # fit colunas 1 e 2 do index
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding categorical data
+# Colunas que tem um mesmo padrão (ex: 10 linhas com 3 tipos de países vão ser divididos
+# em 0, 1 e 2 e depois vão ter 3 valores (0, 0, 1) para o ml não achar que um valor é
+# maior que o outro apenas por causa da categoria dele
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# X = colunas independentes e X[:, ] = todas as linhas da coluna 0
+labelEncoder_X = LabelEnconder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+#OneHotEncoder vai transformar minhas categorias em uma tabela de 0, 0, 1
+oneHotEncoder = OneHotEncoder(categorial_features = [0])
+# lembrar de formatar os valores em .0f 
+X = oneHotEncoder.fit_transform(X).toarray()
+
+# Y = coluna dependente
+labelEncoder_Y = LabelEncoder()
+# como essa coluna só são dois valores (0 e 1) não precisa do onehotencoder.
+Y = labelEncoder_Y.fit_transform(Y)
+
