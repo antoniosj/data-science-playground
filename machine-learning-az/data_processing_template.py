81dc6caeca47b494848af8346af62c08b195ddae
antoniosj <antonio.silvajr23@gmail.com>
2018-12-08 11:46:25 -0300

    update data pre processing template

--- machine-learning-az/data_processing_template.py
+++ machine-learning-az/data_processing_template.py
@@ -15,13 +15,16 @@ X = dataset.iloc[:, :-1].values
 lastColumn = 3
 Y = dataset.iloc[:, lastColumn].values
 
+"""
 # tirar a media para preencher valor faltando
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 # fit colunas 1 e 2 do index
 imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])"""
 
+
+"""
 # Encoding categorical data
 # Colunas que tem um mesmo padrão (ex: 10 linhas com 3 tipos de países vão ser divididos
 # em 0, 1 e 2 e depois vão ter 3 valores (0, 0, 1) para o ml não achar que um valor é
@@ -29,10 +32,10 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 # X = colunas independentes e X[:, ] = todas as linhas da coluna 0
-labelEncoder_X = LabelEnconder()
+labelEncoder_X = LabelEncoder()
 X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
 #OneHotEncoder vai transformar minhas categorias em uma tabela de 0, 0, 1
-oneHotEncoder = OneHotEncoder(categorial_features = [0])
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
 # lembrar de formatar os valores em .0f 
 X = oneHotEncoder.fit_transform(X).toarray()
 
@@ -40,6 +43,16 @@ X = oneHotEncoder.fit_transform(X).toarray()
 labelEncoder_Y = LabelEncoder()
 # como essa coluna só são dois valores (0 e 1) não precisa do onehotencoder.
 Y = labelEncoder_Y.fit_transform(Y)
+"""
+
 # Splitting the dataset into training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+# Feature scalling
+# Standard or Normalization
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
