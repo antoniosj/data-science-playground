457650544630ff7528c4a97db1d9f8184b04a075
antoniosj <antonio.silvajr23@gmail.com>
2018-12-14 01:52:52 -0300

    update train test split

--- linear-regression/multiple_linear_regression.py
+++ linear-regression/multiple_linear_regression.py
@@ -24,15 +24,17 @@ X = oneHotEncoder.fit_transform(X).toarray()
 
 X = X[:, 1:]
 
+
+# Splitting the dataset into training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+
 # Fitting mlr to the training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train) 
 
-
-# Splitting the dataset into training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 # Predicting the test set results
 y_pred = regressor.predict(X_test)
