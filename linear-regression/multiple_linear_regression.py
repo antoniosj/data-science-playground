d05ac81a1190db415e7ea3f04c796308ec963db1
antoniosj <antonio.silvajr23@gmail.com>
2018-12-14 22:56:52 -0300

    update mlr

--- linear-regression/multiple_linear_regression.py
+++ linear-regression/multiple_linear_regression.py
@@ -39,8 +39,21 @@ regressor.fit(X_train, y_train)
 # Predicting the test set results
 y_pred = regressor.predict(X_test)
 
+# select a significance lvl = eg 0,05 - step 1
+
 # Building the optimal model using BACKWARD ELIMINATION
 import statsmodels.formula.api as sm
+
+# fit the full model with all possible predictors
 X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
 X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit() # OLS = Ordinary least square
+# see the summary and consider the predictor with the highest p-value. if p > sign lvl remove.
+regressor_OLS.summary()
+
+# removing this 0.99 predictor and fit model without this variable
+X_opt = X[:, [0, 3]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
